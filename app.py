232ff70fc0073eacb3abcb02d30eb76d89f35307
strodenat <strodenat@gmail.com>
2024-06-02 00:44:51 -0400

    Ensure textbasedgame module is included

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-# app.py
-
 from flask import Flask, render_template, request, jsonify
-from textbasedgame import process_input, game_intro
+from TextBasedGame import process_input, game_intro
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    intro = game_intro()
-    return render_template('index.html', intro=intro)
+    return render_template('index.html')
 
 @app.route('/game', methods=['POST'])
 def game():
-    user_input = request.form['input']
+    user_input = request.form.get('user_input')
     response = process_input(user_input)
-    return jsonify({'output': response})
+    return jsonify({'response': response})
 
 if __name__ == '__main__':
     app.run(debug=True)
