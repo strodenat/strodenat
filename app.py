5fccd1cda4d10027b1679ac018795b398c6c4577
strodenat <strodenat@gmail.com>
2024-06-02 00:52:01 -0400

    Update game functionality

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request, jsonify
-from TextBasedGame import process_input, game_intro
+from textbasedgame import process_input, game_intro
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', intro=game_intro())
 
 @app.route('/game', methods=['POST'])
 def game():
-    user_input = request.form.get('user_input')
+    user_input = request.form.get('input')
     response = process_input(user_input)
-    return jsonify({'response': response})
+    return jsonify({'output': response})
 
 if __name__ == '__main__':
     app.run(debug=True)
