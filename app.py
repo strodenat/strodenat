488c254afe0bd5fa13bae1ca6206c5e4300238c4
strodenat <strodenat@gmail.com>
2024-06-04 21:31:13 -0400

    Update overall game functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session, jsonify
+from flask import Flask, request, session, jsonify, render_template
 from game.textbasedgame import initialize_game, process_input
 
 app = Flask(__name__)
@@ -7,7 +7,8 @@ app.secret_key = 'your_secret_key'
 @app.route('/')
 def index():
     if 'player' not in session:
-        initialize_game()
+        with app.app_context():
+            initialize_game()
     return render_template('index.html', intro=session['rooms'][session['player']['location']]['description'])
 
 @app.route('/game', methods=['POST'])
